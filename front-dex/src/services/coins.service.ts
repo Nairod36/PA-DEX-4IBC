c4f7244e010df2203b6f078306bbaae1077d24f7
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-01-14 11:47:33 +0100

    Add time selector

--- front-dex/src/services/coins.service.ts
+++ front-dex/src/services/coins.service.ts
@@ -3,7 +3,19 @@ import { ETime, ICoin } from '../dto'
 
 export class CoinService {
     static async getCoin(coin: string, time: ETime, token?: CancelToken): Promise <ICoin|null>{
-        const response = await axios.get(`https://min-api.cryptocompare.com/data/v2/${time}?fsym=${coin}&tsym=USD&limit=24`,{
+        let limit:number
+        switch(time){
+            case ETime.Day:
+                limit = 24
+                break
+            case ETime.Hour:
+                limit = 60
+                break
+            case ETime.Minute:
+                limit = 60
+                break
+        }
+        const response = await axios.get(`https://min-api.cryptocompare.com/data/v2/${time}?fsym=${coin}&tsym=USD&limit=${limit}`,{
             cancelToken:token,
             headers:{
                 "Authorization":process.env.REACT_APP_CRYPTO_COMPARE_API_TOKEN
