0448369c453204adc7f80e376c5e14fad3549393
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-03-04 14:31:03 +0100

    fix env variable

--- api-layer-go/main.go
+++ api-layer-go/main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
-    "database/sql"
-    "log"
-    "net/http"
-    "encoding/json"
-    "github.com/gorilla/mux"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
 
 func main() {
     var err error
-    connStr := "user=myuser password=mypassword dbname=mydatabase sslmode=disable"
+
+    DB_USER, ok := os.LookupEnv("DB_USER")
+    if !ok {
+        log.Fatal("DB_USER must be set and non-empty")
+    }
+
+    DB_PASSWORD, ok := os.LookupEnv("DB_PASSWORD")
+    if !ok {
+        log.Fatal("DB_PASSWORD must be set and non-empty")
+    }
+
+    DB_NAME, ok := os.LookupEnv("DB_NAME")
+    if !ok {
+        log.Fatal("DB_NAME must be set and non-empty")
+    }
+
+    connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",DB_USER,DB_PASSWORD,DB_NAME)
     db, err = sql.Open("postgres", connStr)
     if err != nil {
         log.Fatal(err)
