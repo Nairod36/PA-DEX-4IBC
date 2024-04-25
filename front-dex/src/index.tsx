b86b3c71131a9463b352e56fa8bb68444893eb23
Nairod36 <moulin.solal@gmail.com>
2024-04-25 16:04:34 +0200

    Add RaindowKit Wallet

--- front-dex/src/index.tsx
+++ front-dex/src/index.tsx
@@ -2,14 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+//TODO
+const config = getDefaultConfig({
+  appName: 'PA-DEX-24',
+  projectId: 'ad0fb90e38e7329c5dd09dc17bb948ba',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>        
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
