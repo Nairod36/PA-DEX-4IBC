cb1e25425bb8386ad1b048f6e1519aa7233c160f
MatthieuLvsr <72619668+MatthieuLvsr@users.noreply.github.com>
2024-05-27 23:00:24 +0200

    fix rainbowkit

---
+++ front-dex/src/RainbowKitConfig.js
@@ -0,0 +1,31 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export function MyApp({ children }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        {children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
